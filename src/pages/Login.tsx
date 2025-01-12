5df9b17f6c7c944315db16537c5101abc3f6d808
hmmasudsarder <hmmasudsardar@gmail.com>
2025-01-12 11:59:31 +0600

    added auth User login successfully

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,47 @@
+import { Button } from 'antd';
+import { useForm } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
+import { setUser } from '../redux/features/auth/authSlice';
+import { useAppDispatch } from '../redux/features/hook';
+import { verifyToken } from '../utils/verifyToken';
 
 const Login = () => {
-  return (
-    <div>
-      This is Login page
-    </div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: 'A-0001',
+      password: 'admin123',
+    },
+  });
 
-export default Login
+  const [login] = useLoginMutation();
+
+  const onSubmit = async (data: { userId: string; password: string; }) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    const res = await login(userInfo).unwrap();
+    console.log(res)
+    const user = verifyToken(res.data.accessToken);
+    console.log(user)
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register('userId')} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register('password')} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
+};
+
+export default Login;
