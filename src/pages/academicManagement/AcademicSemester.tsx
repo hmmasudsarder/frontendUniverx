6fe6daa76c85160caf5c37e9c2209fd94c52a3fe
hmmasudsarder <hmmasudsardar@gmail.com>
2025-01-17 13:02:14 +0600

    added academicSemsester table

--- src/pages/academicManagement/AcademicSemester.tsx
+++ src/pages/academicManagement/AcademicSemester.tsx
@@ -1,14 +1,126 @@
-import { useGetAllSemesterQuery } from "../../redux/features/academicSemester/AcademicSemesterApi"
+import { Button, Table, TableColumnsType, TableProps } from 'antd';
+import { useState } from 'react';
+import { TAcademicSemester } from '../../types/academicManagement.type';
+import { TQueryParam } from '../../types/glogbal';
+import { useGetAllSemestersQuery } from '../../redux/features/admin/academicManagement.api';
 
+
+export type TTableData = Pick<
+  TAcademicSemester,
+  'name' | 'year' | 'startMonth' | 'endMonth'
+>;
 
 const AcademicSemester = () => {
-    const {data} = useGetAllSemesterQuery(undefined)
-    console.log(data)
-  return (
-    <div>
-      <h1>This is Academic Semester</h1>
-    </div>
-  )
-}
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+  const {
+    data: semesterData,
+    isLoading,
+    isFetching,
+  } = useGetAllSemestersQuery(params);
 
-export default AcademicSemester
+  console.log({ isLoading, isFetching });
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, name, startMonth, endMonth, year }) => ({
+      key: _id,
+      name,
+      startMonth,
+      endMonth,
+      year,
+    })
+  );
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'name',
+      filters: [
+        {
+          text: 'Autumn',
+          value: 'Autumn',
+        },
+        {
+          text: 'Fall',
+          value: 'Fall',
+        },
+        {
+          text: 'Summer',
+          value: 'Summer',
+        },
+      ],
+    },
+    {
+      title: 'Year',
+      key: 'year',
+      dataIndex: 'year',
+      filters: [
+        {
+          text: '2024',
+          value: '2024',
+        },
+        {
+          text: '2025',
+          value: '2025',
+        },
+        {
+          text: '2026',
+          value: '2026',
+        },
+      ],
+    },
+    {
+      title: 'Start Month',
+      key: 'startMonth',
+      dataIndex: 'startMonth',
+    },
+    {
+      title: 'End Month',
+      key: 'endMonth',
+      dataIndex: 'endMonth',
+    },
+    {
+      title: 'Action',
+      key: 'x',
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const onChange: TableProps<TTableData>['onChange'] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === 'filter') {
+      const queryParams: TQueryParam[] = [];
+
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: 'name', value: item })
+      );
+
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: 'year', value: item })
+      );
+
+      setParams(queryParams);
+    }
+  };
+
+  return (
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+    />
+  );
+};
+
+export default AcademicSemester;
